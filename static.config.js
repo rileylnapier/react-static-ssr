1ab25455c19b61c962d49338c0f0ded878fa26fb
riley napier <rileylnapier@rileys-MacBook-Pro.local>
2017-11-24 21:39:45 -0800

    fix for ssr

--- static.config.js
+++ static.config.js
@@ -1,4 +1,26 @@
 import nodeExternals from 'webpack-node-externals'
+import { clearChunks, flushChunkNames } from 'react-universal-component/server'
+import flushChunks from 'webpack-flush-chunks'
+import webpack from 'webpack'
+
+const fs = require('fs')
+const path = require('path')
+
+const res = p => path.resolve(__dirname, p)
+
+const nodeModules = res('./node_modules')
+
+// if you're specifying externals to leave unbundled, you need to tell Webpack
+// to still bundle `react-universal-component`, `webpack-flush-chunks` and
+// `require-universal-module` so that they know they are running
+// within Webpack and can properly make connections to client modules:
+const externals = fs
+  .readdirSync(nodeModules)
+  .filter(x => !/\.bin|react-universal-component|webpack-flush-chunks/.test(x))
+  .reduce((externals, mod) => {
+    externals[mod] = mod
+    return externals
+  }, {})
 
 export default {
   getSiteProps: () => ({
@@ -19,6 +41,16 @@ export default {
     },
   ],
   webpack: (config, { stage, defaultLoaders }) => {
-    config.externals = []
+    if (stage === 'node') {
+      config.externals = externals
+
+      config.plugins.push(
+        new webpack.optimize.LimitChunkCountPlugin({
+          maxChunks: 1,
+        }),
+      )
+    }
+
+    console.log(config.target)
   },
 }
